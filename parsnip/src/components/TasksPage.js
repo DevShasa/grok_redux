d03384cefeedefbeda54d742702e86acf868eb6b
Shasa <shasa.thuo@gmail.com>
2022-01-09 22:06:07 +0300

    intermediate

--- parsnip/src/components/TasksPage.js
+++ parsnip/src/components/TasksPage.js
@@ -4,6 +4,31 @@ import Tasklist from './Tasklist';
 const TASK_STATUSES = ['Unstarted', 'In Progress', 'Complete']
 // Each task in the list is replaced with <Tasklist> containing all the tasks with that tag 
 export default class TasksPage extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            showNewCardForm: false,
+            title: '',
+            description: ''
+        };
+    }
+
+    onTitleChange = (e)=>{
+        this.setState({title: e.target.value})
+    }
+    
+    onDescriptionChange = (e) =>{
+        this.setState({description: e.target.value})
+    }
+
+    resetForm(){
+        this.setState({
+            showNewCardForm: false,
+            title: '',
+            description: '',
+        });
+    }
+    
     renderTasksList(){
         const {tasks} = this.props;
         return TASK_STATUSES.map(status =>{
