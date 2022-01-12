39a5436de8c37913aad6d0f6d77a3e5bf9598b00
Shasa <shasa.thuo@gmail.com>
2022-01-12 18:19:42 +0300

    Integrated select element

--- parsnip/src/components/Task.js
+++ parsnip/src/components/Task.js
@@ -1,4 +1,10 @@
 import React from 'react';
+const TASK_STATUSES = [
+    "Unstarted",
+    "In Progress",
+    "Complete"
+]
+
 const Task = (props)=>{
     return(
         <div className="task">
@@ -6,11 +12,35 @@ const Task = (props)=>{
                 <span className="header-text">
                     {props.task.title}
                 </span>
+
+                <select value = {props.task.status} onChange={onStatusChange}>
+                    {TASK_STATUSES.map(status =>{
+                        let selected = false 
+                        if(status === props.task.status){
+                            selected = true
+                        }
+                        return(
+                            <option 
+                                key={status} 
+                                value = {status}
+                                {...selected} 
+                            >
+                                {status}
+                            </option>
+                        )
+                    })}
+                </select>
             </div>
             <div className="task-body">
                 {props.task.description}
             </div>
         </div>
     )
+    
+    function onStatusChange(e){
+        console.log(props.task.id, e.target.value)
+        //props.onStatusChange(props.task.id, e.target.value)
+    }
+
 };
 export default Task;
