d6eded1ffeaca2f31aeff90d4eae8bcc624bc38d
Shasa <shasa.thuo@gmail.com>
2022-01-13 22:04:30 +0300

    redux devtools

--- parsnip/src/redux/index.js
+++ parsnip/src/redux/index.js
@@ -1,5 +1,8 @@
 import { createStore } from 'redux'
 import tasks from './reducer'
 
-const store = createStore(tasks)
+const store = createStore(
+    tasks,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
 export default store
